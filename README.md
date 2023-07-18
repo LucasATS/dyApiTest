ae7c6d5d415ec8ce31ea1082c0b17a95bcd18f23
Lucas Almeida Tiburtino da Silva <64665422+LucasATS@users.noreply.github.com>
2023-07-17 21:14:24 -0400

    Criação do site e add de varias tasks

--- README.md
+++ README.md
@@ -1,2 +1,44 @@
-# dyApiTest
- Sistema de testes de API - simples
+# dyApiTest: Sistema de teste de APIs
+
+### Um modelo de site feito em React Unpkg.
+
+### Ajude-nos, a melhorar o site 🤩❤️
+
+    FEITO POR https://github.com/LucasATS
+
+[![preview](./.github/preview.png)](https://lucasats.github.io/LINK/)
+
+[🔗 ACESSE O PROJETO ](https://lucasats.github.io/LINK/)
+
+## __📂 PLAY__
+> Apenas edite o index.html
+
+> Se vc tiver python, use python.exe -m http.server 8000
+
+## __🎯 METAS__
+- [X] Ser simples
+- [ ] Reconstruir lógica para o header
+- [ ] Permitir enviar arquivos
+- [ ] Construir exemplo de código (javascript(fetch), javascript(axios), python...)
+- [ ] Thema dracula - dark, salvar preferencia do thema nos cookies
+
+<!--
+## __❤ AGRADECIMENTOS__
+[@<NOME>](<LINK>) "<MENSAGEM>"
+-->
+
+## __🛠 TECNOLOGIAS__ 
+
+    ![JavaScript](https://img.shields.io/badge/JavaScript-323330?style=for-the-badge&logo=javascript&logoColor=F7DF1E)
+    ![React](https://img.shields.io/badge/React-20232A?style=for-the-badge&logo=react&logoColor=61DAFB)
+    ![CSS](https://img.shields.io/badge/CSS3-1572B6?style=for-the-badge&logo=css3&logoColor=white)
+    ![HTML5](https://img.shields.io/badge/HTML5-E34F26?style=for-the-badge&logo=html5&logoColor=white)
+
+## __😘 CONTATO__ <ALTERE APENAS OS LINKS DOS HREF>
+<p align="left">
+  <a href="https://github.com/LucasATS/"><img src="https://img.shields.io/badge/GitHub-100000?style=for-the-badge&amp;logo=github&amp;logoColor=white" alt="Github"></a>
+  <a href="https://www.instagram.com/lukaolmd/"><img src="https://img.shields.io/badge/Instagram-E4405F?style=for-the-badge&amp;logo=instagram&amp;logoColor=white" alt="instagram"></a>
+  <a href="https://www.linkedin.com/in/lucas-almeida-tiburtino-da-silva/"><img src="https://img.shields.io/badge/LinkedIn-0077B5?style=for-the-badge&amp;logo=linkedin&amp;logoColor=white" alt="linkedin"></a>
+  <a href="mailto:lucas.almida.da.silva@gmail.com"><img src="https://img.shields.io/badge/Gmail-D14836?style=for-the-badge&logo=gmail&logoColor=white" alt="gmail"></a> 
+  <a href="https://discord.com/channels/@Lucas%20ATS#9901"><img src="https://img.shields.io/badge/Discord-5865F2?style=for-the-badge&logo=discord&logoColor=white" alt="discord"></a>  
+</p>
---
+++ index.js
@@ -0,0 +1,119 @@
+const lerLink = () => {
+    var data = window.location.search;
+
+    if (data === '') return null;
+
+    if (data.startsWith("?")) {
+        data = data.substring(1);
+    }
+
+    var decodedString = atob(data);
+
+    try {
+        return objeto = JSON.parse(decodedString);
+    } catch (error) {
+        console.error('Erro ao converter a string JSON:', error);
+        return null;
+    }
+}
+
+
+const gerarLink = async (dados) => {
+    const jsonString = JSON.stringify(dados);
+
+    let encoder = new TextEncoder();
+    let dataBytes = encoder.encode(jsonString);
+
+    let base64String = btoa(String.fromCharCode.apply(null, dataBytes));
+
+    const novoLink = window.location.origin + '/?' + base64String;
+
+    await navigator.clipboard.writeText(window.location.origin + '/?' + base64String)
+        .then(() => {
+            alert("String copiada para a área de transferência!");
+        })
+        .catch(error => {
+            alert("Erro ao copiar para a área de transferência:", error);
+        });
+
+    window.location.href = novoLink;
+}
+
+const submit = async ({
+    url = '',
+    method = '',
+    headers = '',
+    mode = '',
+    body = '',
+}) => {
+    let options = {
+        method: method,
+        // headers: JSON.stringify(formatarJson(headers)),
+        headers: JSON.parse(headers),
+    };
+
+    if (mode !== '') {
+        if (mode !== null) {
+            Object.assign(options, { mode }); //mode: mode
+        }
+    }
+
+    if (method === 'GET' || method === 'HEAD') {
+        delete options.body;
+    } else {
+        options.body = JSON.stringify(formatarJson(body));
+    }
+
+    const response = await fetch(url, options);
+
+    console.log(response);
+    return response;
+}
+
+const formatarJson = (txt) => {
+    let texto = txt;
+
+    if (texto === null || texto === '') {
+            return '';        
+    }
+
+    try {
+
+        texto = texto.replace(/([{,]\s*)([A-Za-z0-9_\-]+)\s*:/g, (match, p1, p2) => {
+            return p1 + '"' + p2.trim() + '":';
+        });
+
+        texto = texto.replace(/:\s*('([^']*)'|"([^"]*)")/g, (match, p1, p2, p3) => {
+            const valor = p2 !== undefined ? p2 : p3;
+            return ': "' + valor.trim() + '"';
+        });
+
+        texto = texto.replace(/,\s*([\n\r]+\s*)*}$/g, '}');
+
+        const objeto = JSON.parse(texto);
+
+        return objeto;
+    } catch (error) {
+        alert('Erro ao converter o body em objeto 🙁');
+        console.error('Erro ao converter o body em objeto 🙁', error);
+        throw error;
+    }
+};
+
+// const submit = async ({
+//     url = '',
+//     method = '',
+//     headers = '',
+//     mode = '',
+//     body = '',
+// }) => {
+
+//     const response = await fetch('http://localhost:3001/api/usuario/criar', {
+//         method: "POST", // or 'PUT'
+//         headers: { "Content-Type": "application/json" },
+//         // mode: "*",
+//         body: JSON.stringify(dados),
+//     });
+
+//     console.log(response);
+// }
